7446c701a8ab935db9405aef3947efa13af17885
huala-fun <huala.fun@outlook.com>
2024-05-08 23:49:10 +0800

    fix

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -2,6 +2,7 @@ import { redis, genCodeShareKey } from "@/lib/redis";
 import { v4 as uuidv4 } from "uuid";
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
 
 export const POST = async (req: NextRequest) => {
   //   const themes = await redis.get("themes");
@@ -10,6 +11,27 @@ export const POST = async (req: NextRequest) => {
   const uuid = uuidv4();
   const keyPrefix = genCodeShareKey(uuid);
   await redis.set(keyPrefix, JSON.stringify(json));
+
+
+
+
+  //  prisma.codeShare
+  //   .create({
+  //     data: {
+  //       uuid,
+  //       code: json.code,
+  //       theme: json.theme,
+  //       session: {
+  //         connect: {
+  //           id: session.user.id,
+  //         },
+  //       },
+  //     },
+  //   })
+
+
+
+
   return NextResponse.json({
     code: 200,
     message: "success",
